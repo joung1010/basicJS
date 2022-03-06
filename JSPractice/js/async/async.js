f9aa7ad4c552206c9260feffd5d1d6b44b0b2332
joung1010 <79488695+joung1010@users.noreply.github.com>
2022-03-06 14:06:22 +0900

    JS: await

--- JSPractice/js/async/async.js
+++ JSPractice/js/async/async.js
@@ -49,4 +49,81 @@ async function fetchUse3r() {
 const user3 = fetchUser2();
 user3.then(console.log);
 
+// await( async 가 붙은 함수안에서만 사용가능 ) -> await가 붙은 작업을 끝날때까지 기다려줘!
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve,ms));
+}
+
+async function getApple() {
+    await delay(1000);
+    return'apple';
+}
+
+async function getBanana() {
+    await delay(3000);
+    return'banana';
+}
+
+function noAsyncKeywordBanana() {
+    return delay(1000).then(() =>'banana');
+}
+
+const apple = getApple();
+const banana = getBanana();
+console.log(`apple: ${apple}`) // 프로미스가 return
+console.log(`banana: ${banana}`)// 프로미스가 return
+console.log(noAsyncKeywordBanana())// 프로미스가 return
+
+function prickFruits1() {
+    return getApple()
+        .then(apple => getBanana().then(banana => `${apple},${banana}`));
+}
+
+async function prickFruits2() {
+    const apple = await getApple();
+    const banana = await getBanana();
+    // 두개의 작업은 서로 연관성이 없기때문에 apple 작업이 끝난뒤에 banana가 실행될 이유가 없다
+    // apple이 실행되는데 banana가 필요없고 banana가 실행되는데 apple이 필요없다
+    // console.log(apple);
+    // console.log(banana);
+    return `${apple},${banana}`;
+}
+// 개선해보기
+async function prickFruits3() {
+    const applePromise =  getApple();
+    const bananaPromise = getBanana();
+    const apple = await applePromise;
+    const banana = await bananaPromise;
+    // promise는 생성과 동시에 실행된다.
+    // console.log(apple);
+    // console.log(banana);
+    return `${apple},${banana}`;
+}
+
+// promise의 유용한 API 사용해보기
+function pickAllFruits() {
+    return Promise.all([getApple(), getBanana()])
+        // all 함수는 인자로 Promise 배열로 전달하면 해당 배열 Promise 값들을 모두 받아 모아준다
+        .then(fruits => {
+            console.log(fruits);    // [apple, banana]
+            return fruits.join('+');
+        });
+}
+
+function pickOnlyOne() {
+    return Promise.race([getApple(), getBanana()]);
+    // race함수는 Promise 배열중 가장 첫번째로 값을 return하는 Promise의 값을 return한다
+}
+
+
+prickFruits1().then(console.log);
+prickFruits2().then(console.log);
+prickFruits3().then(console.log);
+
+
+pickAllFruits().then(console.log);
+pickOnlyOne().then(console.log);
+
+
 
