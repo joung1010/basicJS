5597994a305c16dad424cd9632c93af025ed19dd
joung1010 <79488695+joung1010@users.noreply.github.com>
2022-03-07 21:01:49 +0900

    APIs : window 사이즈 실습 준비

---
+++ APIs/js/windowSize.js
@@ -0,0 +1,5 @@
+//Window Size
+//1. Window Screen : 브라우저 밖, 즉 모니터 사이즈
+//2. Window.outer: outer width, outer height -> 브라우저의 사이즈
+//3, Window.inner : 페이지가 표시되는 전체부분(스크롤 포함)
+//4. DocumentElement.clientWidth: 전체적인 페이지(스크롤 제외)
