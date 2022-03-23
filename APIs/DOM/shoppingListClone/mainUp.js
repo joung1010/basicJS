1a3cd47d80d5f7ea9b3ccc7df9125114d62fcf92
joung1010 <79488695+joung1010@users.noreply.github.com>
2022-03-23 20:21:32 +0900

    ShoppingList 이벤트  form 태그 submit 이벤트 활용

--- APIs/DOM/shoppingListClone/mainUp.js
+++ APIs/DOM/shoppingListClone/mainUp.js
@@ -1,10 +1,23 @@
 const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
+const form = document.querySelector('.new-form');
 
 /**
  * 동적으로 요소 추가 함수
  * */
+
+form.addEventListener('submit',event=>{
+    event.preventDefault();
+   onAdd();
+   // 버튼 클릭과 enter를 치면 값이 추가되었다가 사라진다
+    // 이때 버튼 태그에 타입을 명시해줘서 이 버튼을 클릭했을때 sumit이 발생할 수 있게해준다.
+    // why?? submit 이벤트가 발생화면 자동으로 브라우저가 페이지를 다시 로딩한다.
+    // 그래서 그런 자동적인 해동을 막아줄 필요가 있다.
+});
+//참고
+// 튜토리얼: https://developer.mozilla.org/en-US/docs/Learn/Forms
+// form 태그: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/form
 function onAdd() {
     const text = input.value;
     if (text === '') {
@@ -46,7 +59,7 @@ items.addEventListener('click', event => {
     }
 });
 
-addBtn.addEventListener('click', () => {
+/*addBtn.addEventListener('click', () => {
     onAdd();
 });
 // keypress Deprecated됨
@@ -58,7 +71,7 @@ input.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
         onAdd();
     }
-});
+});*/
 /*
 *   keyDown 과 keyUp의 차이점
 *   keyDown 은 사용자가 키보드를 눌렀을때 바로 발생하는 이벤트
@@ -66,3 +79,7 @@ input.addEventListener('keydown', (event) => {
 *   keyUp 은 키보드를 눌렀다가 땠을때 발생
 *       -> 키보드가 눌린후에 어떤 작업을 처리하고 싶을때 사용
 * */
+
+// 지금 우리가 input text에 enter 키 또는 버튼을 클릭해서 리스트에 등록했다. 이를 하나로 합쳐보자 form  태그를 활용하자
+// form 태그 안에 있으면 클릭과 키다운을 별도로 처리하지 않아도
+// 사용자가 enter를 치거나 form안에 있는 버튼을 누르면 자동으로 submit이벤트가 발생한다
